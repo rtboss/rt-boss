4ec143b2a0a44f4617ee9c68e971ba80b344d378
rtboss <rtboss@naver.com>
2013-08-08 23:14:04 +0900

    Message Queue 외부 활당 방식으로 변경.

--- Boss/Boss_Q_Msg.c
+++ Boss/Boss_Q_Msg.c
@@ -28,48 +28,47 @@
 /*===========================================================================
     B O S S _ M S G _ Q _ I N I T
 ---------------------------------------------------------------------------*/
-void Boss_msg_q_init(boss_msg_q_t *msg_q, boss_tcb_t *p_tcb, boss_sigs_t sig)
+void Boss_msg_q_init(boss_msg_q_t *msg_q, _msg_fifo_t *p_fifo, boss_uptr_t fifo_bytes,
+                                              boss_tcb_t *p_tcb, boss_sigs_t sig)
 {
   BOSS_ASSERT(msg_q->owner_tcb == _BOSS_NULL);
   BOSS_ASSERT(msg_q->msg_fifo == _BOSS_NULL);
   
   msg_q->owner_tcb  = p_tcb;
   msg_q->msg_sig    = sig;
-  msg_q->msg_fifo   = _BOSS_NULL;
+  
+  msg_q->msg_fifo   = p_fifo;
+  msg_q->fifo_max   = fifo_bytes / sizeof(_msg_fifo_t);
+  msg_q->fifo_count = 0;
+  msg_q->fifo_put   = 0;
+  msg_q->fifo_get   = 0;
 }
 
 
 /*===========================================================================
     B O S S _ M S G _ S E N D
 ---------------------------------------------------------------------------*/
-void Boss_msg_send(boss_msg_q_t *msg_q, msg_cmd_t m_cmd, boss_uptr_t param)
+boss_reg_t Boss_msg_send(boss_msg_q_t *msg_q, msg_cmd_t m_cmd, boss_uptr_t param)
 {
-  _msg_fifo_t *p_msg;
-  
-  p_msg = Boss_malloc(sizeof(_msg_fifo_t));
-  
-  p_msg->m_cmd  = m_cmd;
-  p_msg->param  = param;
-  p_msg->next   = _BOSS_NULL;
+  boss_reg_t send = (boss_reg_t)_BOSS_FAILURE;
 
   BOSS_IRQ_DISABLE();
-  if(msg_q->msg_fifo == _BOSS_NULL)
+  if(msg_q->fifo_count < msg_q->fifo_max)
   {
-    msg_q->msg_fifo = p_msg;
-  }
-  else
-  {
-    _msg_fifo_t *p_tail = msg_q->msg_fifo;
-
-    while(p_tail->next != _BOSS_NULL)
-    {
-      p_tail = p_tail->next;
-    }
-    p_tail->next = p_msg;             /* Å¥ Ãß°¡ */
+    boss_u08_t put_idx = msg_q->fifo_put;
+    
+    msg_q->msg_fifo[put_idx].m_cmd  = m_cmd;
+    msg_q->msg_fifo[put_idx].param  = param;
+    msg_q->fifo_put = ++put_idx % msg_q->fifo_max;
+    
+    msg_q->fifo_count++;
+    send = _BOSS_SUCCESS;
   }
   BOSS_IRQ_RESTORE();
-  
+
   Boss_send(msg_q->owner_tcb, msg_q->msg_sig);
+
+  return send;
 }
 
 
@@ -80,22 +79,19 @@ msg_cmd_t Boss_msg_receive(boss_msg_q_t *msg_q, boss_uptr_t *p_param)
 {
   msg_cmd_t m_cmd = M_CMD_EMPTY;
 
-  BOSS_ASSERT(msg_q->owner_tcb == Boss_self());
-
-  if(msg_q->msg_fifo != _BOSS_NULL)
+  BOSS_IRQ_DISABLE();
+  if(msg_q->fifo_count != 0)
   {
-    _msg_fifo_t *p_msg;
-    
-    BOSS_IRQ_DISABLE();
-    p_msg = msg_q->msg_fifo;
-    msg_q->msg_fifo = p_msg->next;
-    BOSS_IRQ_RESTORE();
+    boss_u08_t get_idx = msg_q->fifo_get;
 
-    m_cmd     = p_msg->m_cmd;
-    *p_param = p_msg->param;
-    
-    Boss_mfree(p_msg);
+    m_cmd     = msg_q->msg_fifo[get_idx].m_cmd;
+    *p_param  = msg_q->msg_fifo[get_idx].param;
+
+    msg_q->fifo_get = ++get_idx %  msg_q->fifo_max;
+
+    msg_q->fifo_count--;
   }
+  BOSS_IRQ_RESTORE();
 
   return m_cmd;
 }
--- Boss/Boss_Q_Msg.h
+++ Boss/Boss_Q_Msg.h
@@ -19,8 +19,6 @@
 typedef struct _msg_fifo_struct {
   msg_cmd_t           m_cmd;
   boss_uptr_t         param;
-  
-  struct _msg_fifo_struct *next;
 } _msg_fifo_t;
 
 
@@ -29,13 +27,18 @@ typedef struct {
   boss_sigs_t   msg_sig;
 
   _msg_fifo_t   *msg_fifo;
+  boss_u08_t    fifo_max;
+  boss_u08_t    fifo_count;
+  boss_u08_t    fifo_put;
+  boss_u08_t    fifo_get;
 } boss_msg_q_t;
 
 /*===========================================================================*/
 /*                            FUNCTION PROTOTYPES                            */
 /*---------------------------------------------------------------------------*/
-void Boss_msg_q_init(boss_msg_q_t *msg_q, boss_tcb_t *p_tcb, boss_sigs_t sig);
-void Boss_msg_send(boss_msg_q_t *msg_q, msg_cmd_t m_cmd, boss_uptr_t param);
+void Boss_msg_q_init(boss_msg_q_t *msg_q, _msg_fifo_t *p_fifo, boss_uptr_t fifo_bytes,
+                                            boss_tcb_t *p_tcb, boss_sigs_t sig);
+boss_reg_t Boss_msg_send(boss_msg_q_t *msg_q, msg_cmd_t m_cmd, boss_uptr_t param);
 msg_cmd_t Boss_msg_receive(boss_msg_q_t *msg_q, boss_uptr_t *p_param);
 
 #endif  /* _BOSS_MSG_Q_H_ */
