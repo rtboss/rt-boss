b11fb813494ffd33b1973ef384d54ffc922662f4
rtboss <rtboss@naver.com>
2013-09-23 23:15:39 +0900

    메시지 큐 관련 기능 추가. Boss_msg_wait() / Boss_msg_send_front() 메시지 수신시 구조체로 반환하도록함.

--- Boss/Boss_Q_Msg.h
+++ Boss/Boss_Q_Msg.h
@@ -16,29 +16,50 @@
 /*===========================================================================*/
 /*                      DEFINITIONS & TYPEDEFS & MACROS                      */
 /*---------------------------------------------------------------------------*/
-typedef struct _msg_fifo_struct {
-  msg_cmd_t           m_cmd;
-  boss_uptr_t         param;
-} _msg_fifo_t;
+typedef enum {
+  MSG_Q_PRIORITY  = 0x00,     /* 우선순위 */
+  MSG_Q_FIFO      = 0x01,     /* 대기순서 */
+} _msg_q_opt_t;
 
 
 typedef struct {
-  boss_tcb_t    *owner_tcb;
-  boss_sigs_t   msg_sig;
+  msg_cmd_t       m_cmd;
+  boss_uptr_t     param;
+} boss_msg_t;
 
-  _msg_fifo_t   *msg_fifo;
+
+typedef struct _msg_wait_struct {
+  struct _msg_wait_struct   *prev;      /* msg wait list link */
+  struct _msg_wait_struct   *next;
+
+  boss_tcb_t    *p_tcb;
+  boss_msg_t    msg;
+} _msg_wait_t;
+
+
+typedef struct {
+  _msg_wait_t   *wait_list;
+  
+  boss_msg_t    *msg_fifo;
   boss_u08_t    fifo_max;
   boss_u08_t    fifo_count;
-  boss_u08_t    fifo_put;
-  boss_u08_t    fifo_get;
+  boss_u08_t    fifo_head;
+  boss_u08_t    fifo_tail;
+  
+  _msg_q_opt_t  q_opt;
 } boss_msg_q_t;
+
 
 /*===========================================================================*/
 /*                            FUNCTION PROTOTYPES                            */
 /*---------------------------------------------------------------------------*/
-void Boss_msg_q_init(boss_msg_q_t *msg_q, _msg_fifo_t *p_fifo, boss_uptr_t fifo_bytes,
-                                            boss_tcb_t *p_tcb, boss_sigs_t sig);
+void Boss_msg_q_init(boss_msg_q_t *msg_q, boss_msg_t *p_fifo,
+                                      boss_uptr_t fifo_bytes, _msg_q_opt_t q_opt);
+
 boss_reg_t Boss_msg_send(boss_msg_q_t *msg_q, msg_cmd_t m_cmd, boss_uptr_t param);
-msg_cmd_t Boss_msg_receive(boss_msg_q_t *msg_q, boss_uptr_t *p_param);
+boss_reg_t Boss_msg_send_front(boss_msg_q_t *msg_q, msg_cmd_t m_cmd, boss_uptr_t param);
+
+boss_msg_t Boss_msg_receive(boss_msg_q_t *msg_q);
+boss_msg_t Boss_msg_wait(boss_msg_q_t *msg_q, boss_tmr_ms_t timeout);
 
 #endif  /* _BOSS_MSG_Q_H_ */
